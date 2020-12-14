b1afb05cc402b3bd6d66c2301f40f0f8f3ba56e8
Andrija <andreccccc12@gmail.com>
2020-12-14 15:54:55 +0100

    footer still not working

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,17 +2,12 @@ import React, { useState, useEffect } from 'react';
 import '../styles/footer.css';
 
 const Footer = () => {
-    // da bi se dugme promenilo mora da se refreshuje stranica
+    // to turn register/login button to my profile button I must refresh
     const [username, setUsername] = useState(false);
-    const [btn, updateBtn] = useState(false);
 
     if(!username && sessionStorage.getItem('username')) {
         setUsername(sessionStorage.getItem('username'));
     }
-
-    useEffect(() => {   
-        updateBtn(true);
-    }, [username]);
 
     return (
         <footer>
@@ -21,7 +16,7 @@ const Footer = () => {
                 <a href="mailto:andrijagajicbusiness@gmail.com"><img src='/images/social_media/email.png' alt="email logo" className="logo"/></a>
                 <a href="https://github.com/gajojr"><img src='/images/social_media/github.png' alt="github logo" className="logo"/></a>
             </div>
-            {!btn ? 
+            {!username ? 
                 <div className="login-register">
                     <a href="http://localhost:8080/register">
                         <div className="adminButton">
