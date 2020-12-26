94bb96a8278a37bd3f128f3c44ef091148328280
Andrija <andreccccc12@gmail.com>
2020-12-26 20:16:58 +0100

    Added error control to the route

--- api/routes/animation-list.js
+++ api/routes/animation-list.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('./login').User;
 
+router.keepAlive = false;
+
 mongoose.connect('mongodb://localhost:27017/animationsdb');
 
 router.get('/', async(req, res) => {
@@ -28,18 +30,23 @@ router.put('/', async(req, res) => {
 
     // if animation is already liked, then dislike it
     // if it's not liked, then store it in db
-    const user = await User.findOne({ username: req.body.username });
-    if (user.likedAnimations.indexOf(req.body.link) === -1) {
-        user.likedAnimations.push(req.body.link);
-    } else {
-        user.likedAnimations = arrayRemove(user.likedAnimations, user.likedAnimations[user.likedAnimations.indexOf(req.body.link)]);
+
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        if (user.likedAnimations.indexOf(req.body.link) === -1) {
+            user.likedAnimations.push(req.body.link);
+        } else {
+            user.likedAnimations = arrayRemove(user.likedAnimations, user.likedAnimations[user.likedAnimations.indexOf(req.body.link)]);
+        }
+        user.save();
+    } catch (error) {
+        console.log("Error on put request(animation-list)", error);
     }
-    user.save();
 });
 
 function arrayRemove(arr, value) {
     return arr.filter((item) => {
-        return item != value;
+        return item !== value;
     });
 }
 
